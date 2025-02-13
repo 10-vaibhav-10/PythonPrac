1bb2a8e8ee67070a05989765b698b4c639589562
10-vaibhav-10 <mr.vaibhav10@gmail.com>
2025-02-13 19:29:52 +0545

    DictionaryPart

--- Dictionary.py
+++ Dictionary.py
@@ -6,7 +6,12 @@ dict={
     "age":22,
     "destination" :"Australia",
     "Course" : "Master of Data Analytics",
-    "Module": ["AI","IOT"]
+    "Module":{
+        "AI":56,
+        "IOT":76,
+        "Big Data":56
+    }
+    
 }
 print(dict["destination"])
 print(dict)
@@ -14,3 +19,23 @@ print(dict)
 dict["name"]="Rahul"
 print(dict["name"])
 print(dict)
+print(dict["Module"]["IOT"])
+
+#Dictionary Methods
+
+print(dict.keys())  #keys used in dictionary formation
+
+print(dict.values())    #returns value for the keys
+
+print(dict.items())   #returns keys along value
+
+print(dict["name"])
+#Returns value for given key
+print(dict.get("Destination"))  
+
+#update/insert file to dictionary
+#we need to create new dictionary variable and then
+#update it to the old dictionary.
+dict1={"name":"Ram ","gender":"male"}
+dict.update(dict1)
+print(dict) 
